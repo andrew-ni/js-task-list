c21454defac3d1d0e1c80fcb40a4938e45f513c3
andrewni <ajni@ucdavis.edu>
2018-01-08 23:49:26 -0800

    delete and filter working

---
+++ app.js
@@ -0,0 +1,71 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', removeAllTasks);
+  filter.addEventListener('keyup', filterTasks);
+}
+
+function addTask(event) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  } else {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a');
+    link.className = 'fa fa-remove delete-item secondary-content';
+    // link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+  }
+
+  event.preventDefault(); // prevent form submit
+}
+
+function removeTask(event) {
+  // Event delegation
+  if (event.target.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      event.target.parentElement.remove();
+    }
+  }
+}
+
+function removeAllTasks(event) {
+  // const tasks = document.querySelectorAll('.collection .collection-item');
+  // tasks.forEach((task) => {
+  //   task.remove();
+  // })
+
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // taskList.innerHTML = '';
+}
+
+function filterTasks(event) {
+  const str = filter.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach((task) => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(str) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+}
